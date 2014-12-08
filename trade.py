a12ce995fd63abaca279f59affc359bb701633d7
aurko <roy.aurko@gmail.com>
2014-12-08 18:00:03 -0500

    initial commit with buy and query balance functions

---
+++ trade.py
@@ -0,0 +1,57 @@
+import os
+import hashlib
+import hmac
+import urllib.request, urllib.error, urllib.parse
+import time
+import json
+
+#This code sample demonstrates a GET and a POST call to the Coinbase API
+#Before implementation, set environmental variables with the names API_KEY and API_SECRET.
+
+
+def make_request(url, body=None):
+  opener = urllib.request.build_opener()
+  nonce = int(time.time() * 1e6)
+  message = str(nonce) + url + ('' if body is None else body)
+  signature = hmac.new(str.encode(os.environ['API_SECRET']), str.encode(message), hashlib.sha256).hexdigest()
+
+  headers = {'ACCESS_KEY' : os.environ['API_KEY'],
+             'ACCESS_SIGNATURE': signature,
+             'ACCESS_NONCE': nonce,
+             'Accept': 'application/json'}
+
+  # If we are passing data, a POST request is made. Note that content_type is specified as json.
+  if body:
+    headers.update({'Content-Type': 'application/json'})
+    req = urllib.request.Request(url, data=str.encode(body), headers=headers)
+
+  # If body is nil, a GET request is made.
+  else:
+    req = urllib.request.Request(url, headers=headers)
+    response = urllib.request.urlopen(req)
+    return response.read()
+  try:
+    return opener.open(req)
+  except urllib.error.HTTPError as e:
+    print(e)
+    return e
+
+# Example of a GET request, where body is nil.
+
+def buy(amount):
+    param = {
+        'qty' : .01,
+        'commit' : 'false',
+    }
+    req = make_request('https://api.coinbase.com/v1/buys', body = json.dumps(param)).read()
+    req = req.decode("utf-8")
+    print(req)
+
+if __name__ == '__main__':
+    balance = make_request('https://api.coinbase.com/v1/account/balance')
+    balance = balance.decode("utf-8")
+    print(balance)
+    status = make_request('https://api.coinbase.com/v1/orders')
+    status = status.decode("utf-8")
+    print(status)
+    buy(0.01)

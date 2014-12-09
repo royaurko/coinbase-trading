7f284ce9ab25c40e48d3118f7a38b65574e6fbf0
aurko <roy.aurko@gmail.com>
2014-12-09 15:53:31 -0500

    minor

--- strategies.py
+++ strategies.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-import trade
+from trade import list_accounts, query, buy, sell, status
+
+
+if __name__ == '__main__':
+    my_account = list_accounts()
+    log = open('tradelogs', 'w')
+    buy(1.5, log, my_account[0], 'USD')
+    sell(1.5, log, my_account[0], 'USD')

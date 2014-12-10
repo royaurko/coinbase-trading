9e60368c273a20bee46f316f50bd840fe0b462c9
aurko <roy.aurko@gmail.com>
2014-12-10 17:58:51 -0500

    some minor modifications in the case of simulation

--- strategies.py
+++ strategies.py
@@ -8,10 +8,13 @@ from trade import list_accounts, query, buy, sell, exchange_rate
 # floor is the minimum value to which you are willing to let your portfolio drop
 # multiplier is your sensitivity to risk
 # A lower multiplier is more conservative, typically chosen between 3-6
-def cppi(portfolio_value, floor, log, account, multiplier=3, currency='USD', flag='n'):
+def cppi(portfolio_value, floor, current_invested, log, account, multiplier=3, currency='USD', flag='n'):
     target_investment = multiplier * (portfolio_value - floor)
-    current = query(account)
-    current_currency = current * exchange_rate(currency)
+    if flag =='y':
+        current = query(account)
+        current_currency = current * exchange_rate(currency)
+    else:
+        current_currency = current_invested
     if target_investment < 0 and current_currency > 0:
         # If value fell below floor, sell all bitcoins!
         print('Floor breached, selling!')
@@ -29,7 +32,7 @@ def cppi(portfolio_value, floor, log, account, multiplier=3, currency='USD', fla
         else:
             print('Sitting tight')
             write_str = time.strftime("%m.%d.%y %H:%M ", time.localtime())
-            write_str += 'Target investment matches current investment \n'
+            write_str += 'Target investment of ' + str(target_investment) + ' matches current investment \n'
             log.write(write_str.encode("utf-8"))
         return target_investment
     else:
@@ -40,13 +43,18 @@ def cppi(portfolio_value, floor, log, account, multiplier=3, currency='USD', fla
         return 0
 
 
-def cppi_rebalance(portfolio_value, floor, interval, log, account, multiplier=3, currency='USD', flag='n'):
+def cppi_rebalance(portfolio_value, floor, current_invested, interval, log, account, multiplier=3, currency='USD', flag='n'):
     pvalue = portfolio_value
     while True:
-        bitcoin_investment = cppi(pvalue, floor, log, account, multiplier, currency, flag)
+        bitcoin_investment = cppi(pvalue, floor, current_invested, log, account, multiplier, currency, flag)
+        old_rate = exchange_rate(currency)
         cash = pvalue - bitcoin_investment
         time.sleep(interval)
-        pvalue = cash + query(account) * exchange_rate(currency)
+        if flag == 'y':
+            pvalue = cash + query(account) * exchange_rate(currency)
+        else:
+            pvalue = cash + (bitcoin_investment/old_rate) * exchange_rate(currency)
+            current_invested = (bitcoin_investment/old_rate) * exchange_rate(currency)
 
 
 if __name__ == '__main__':
@@ -57,6 +65,10 @@ if __name__ == '__main__':
     interval = int(input('Time interval to rebalance portfolio (in seconds): '))
     currency = input('Enter currency: ')
     initial_pvalue = float(input('Initial value of portfolio (including bitcoin investments if any): '))
+    if flag == 'y':
+        current_invested = query(account) * exchange_rate(currency)
+    else:
+        current_invested = float(input('Amount in ' + currency + ' currently invested in bitcoins: '))
     floor = float(input('Enter floor: '))
     multiplier = float(input('Enter multiplier: '))
-    cppi_rebalance(initial_pvalue, floor, interval, log, my_account[0], multiplier, currency, flag)
+    cppi_rebalance(initial_pvalue, floor, current_invested, interval, log, my_account[0], multiplier, currency, flag)
